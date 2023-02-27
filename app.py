0d20a7099a83b40d37f7b70b5bdedc4df957dda1
DandaUpendarReddy <121006921+DandaUpendarReddy@users.noreply.github.com>
2023-02-27 16:21:35 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("HELLO EVERYONE")
-
-st.markdown(''' I am Danda Upendar Reddy an aspiring Data scientist.\n
-Objective:To secure a responsible career opportunity as a Data Scientist to fully utilize my training and skills, while making a significant contribution to the success of company
-Github:["Follow me on github"](https://github.com/DandaUpendarReddy).\n
-Linkdein:["Connect with me on Linkdein"](https://in.linkedin.com/in/danda-upendar-reddy)''')
